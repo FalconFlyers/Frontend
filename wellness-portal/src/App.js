e2d9e84c5748f6e5d2a0d366dadb189740b69df3
Akanksha Faujdar <67420568+akankshafaujdar1311@users.noreply.github.com>
2022-07-29 00:22:43 +0530

    Creation of Footer with Social-Media handles

--- wellness-portal/src/App.js
+++ wellness-portal/src/App.js
@@ -1,13 +1,14 @@
 
 import './App.css';
 import {NavBar, Header, Events} from './Components';
-
+import {Footer} from './Components';
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Header />
       <Events />
+      <Footer/>
     </div>
   );
 }
--- wellness-portal/src/Components/NavBar/NavBar.js
+++ wellness-portal/src/Components/NavBar/NavBar.js
@@ -8,24 +8,24 @@ import './NavBar.css';
 const navBar = () => {
   return (
     <Navbar bg="primary" variant="dark">
-        <Container>
+      <Container>
         <Navbar.Brand href="#home">
-            <img
-              src={process.env.PUBLIC_URL+"images/Balance_within_you logo.png"}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            //   alt="WP logo"
-            />
-          </Navbar.Brand>
-          <Navbar.Brand href="#home">WP</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Explore</Nav.Link>
-            <Nav.Link href="#pricing">Log In</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+          <img
+            src={process.env.PUBLIC_URL + "images/Balance_within_you logo.png"}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          //   alt="WP logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Brand href="#home">WP</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Explore</Nav.Link>
+          <Nav.Link href="#pricing">Log In</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   )
 }
 
