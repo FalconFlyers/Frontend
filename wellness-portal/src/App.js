00b1c72932d7e540eeac5efa95e4b61c4d5d4907
Vanshika <vanshikab06@gmail.com>
2022-08-01 00:12:33 +0530

    Updated routes

--- wellness-portal/src/App.js
+++ wellness-portal/src/App.js
@@ -1,27 +1,30 @@
 import React from "react";
-import './App.css';
-import {NavBar, Header, Events,LogoBox,Fitness,Yoga} from './Components';
-import FitHead from './Components/Events/FitHead';
-import YogaHead from './Components/Events/YogaHead';
-import {Footer} from './Components';
-import {
-  BrowserRouter,         
-  Routes,
-  Route
-} from "react-router-dom";
+import "./App.css";
+import { NavBar, Header, Events, LogoBox } from "./Components";
+import { Fitness, Yoga } from "./Containers";
+import { Footer } from "./Components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Addevent from "./Containers/Admin/Addevent";
 function App() {
   return (
     <div className="App">
-     
       <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route exact path="/home" element={<><Header/><Events/></>}/>
-        {/* <Route exact path="#home" element={<Events/>}/> */}
-        <Route exact path="/admin" element={<Addevent/>}/>
-      </Routes>
-      <Footer/>
+        <NavBar />
+        <Routes>
+          <Route
+            exact
+            path="/home"
+            element={
+              <>
+                <Header />
+                <Events />
+              </>
+            }
+          />
+          <Route exact path="/admin" element={<Addevent />} />
+        </Routes>
+        <Fitness />
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ wellness-portal/src/Containers/index.js
@@ -0,0 +1,2 @@
+export {default as Fitness} from './User/Fitness/Fitness';
+export {default as Home} from './User/home';
